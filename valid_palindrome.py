81b76ee0c0e084b6ff4107c8c7b1499d57c1a8ec
Tsz Kit Wong <wongt@bu.edu>
2023-05-28 02:08:03 -0400

    Update valid_palindrome.py

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,13 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        str = ""
-        
-        for i in s:
-            if i.isalnum():
-                str += i
-                
-        if str.casefold() == str[::-1].casefold():
-            return True
-        
-        return False
-      
+
+        # change the string to lower case
+        s = s.lower()
+        new_s = ""
+        # remove all non-alphanumeric characters
+        for i in range(len(s)):
+            if s[i].isalnum():
+                new_s += s[i]
+
+        # approach 1, two pointers approach O(n)
+        # l = 0  # front
+        # r = len(new_s)-1  # back
+
+        # while l < r:
+        #     if new_s[l] == new_s[r]:
+        #         l += 1
+        #         r -= 1
+        #     else:
+        #         return False
+
+        # return True
+
+        # approach 2, slicing O(n)
+        return new_s == new_s[::-1]

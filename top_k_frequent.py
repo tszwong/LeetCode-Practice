7e53b38cd015ae97f9b7e68d422b4b1af1814fde
Tsz Kit Wong <wongt@bu.edu>
2023-06-30 18:44:08 -0400

    Create top_k_frequent.py

---
+++ top_k_frequent.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {} # keep count
+        for i in range(len(nums)): # update entries
+            if nums[i] not in count:
+                count[nums[i]] = 1
+            else:
+                count[nums[i]] += 1
+
+        # sort the items in dictionary
+        sort = sorted(count.items(), key=lambda x: x[1]) # sort by the values
+        sorted_d = [item[0] for item in sort] # add them to a list
+
+        # add the k most frequet elements to the output list
+        res = []
+
+        # taking from the back of the list since it was sorted from least to most frequent
+        k_in = len(res)-1
+        for i in range(k):
+            res.append(sorted_d[k_in])
+            k_in -= 1
+
+        return res
+        

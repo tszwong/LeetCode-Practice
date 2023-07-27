3fc4fd1d080fc7a4c205e386a4cf54672da3226f
Tsz Kit Wong <wongt@bu.edu>
2023-07-27 14:20:03 -0400

    Update invert_binary_tree.py

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -8,6 +8,9 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root: return None  # base case
 
+        # swapping before the recursive calls strategy utilizes a preorder traversal
+        # swapping after the recursive calls strategy utilizes a postorder traversal
+
         temp = root.left # use a temporary variable to store one side of the tree
         root.left = root.right # swap left with right
         root.right = temp # swap right with left stored in temp
